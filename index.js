080a7b088cc5bdae80aa5c1a290d8ea60a675207
a5medHub <ayassine.auce@gmail.com>
2024-10-19 22:23:42 +0300

    set the values of the hex inner text and the background of the styling

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ const setColorBtn = document.getElementById("set-color-btn")
 const colorData = []
 const getAllMode = []
 const getAllColors = []
+const hexValues = []
 
-colorSeed.addEventListener("change", (e)=>{
+colorSeed.addEventListener("change", (e) => {
     let hexWithHash = e.target.value
-    let removedHash =  e.target.value.replace(/^#/, '')
+    let removedHash = e.target.value.replace(/^#/, '')
     colorData[0] = removedHash
     colorData[2] = hexWithHash
 })
@@ -18,23 +19,21 @@ colorPicker.addEventListener("change", (e) => {
     colorData[1] = e.target.value
 })
 
-setColorBtn.addEventListener("click", (colorSeed, colorPicker)=>{
-    if(colorData!=null){
-        colorSeed = colorData[0]
-        colorPicker = colorData[1]
+setColorBtn.addEventListener("click", () => {
+    if (colorData != null) {
+        let colorSeed = colorData && colorData[0] ? colorData[0] : '000000';
+        let colorPicker = colorData && colorData[1] ? colorData[1] : 'monochrome';
         generatedColorAPI(colorSeed, colorPicker)
 
     }
 })
 
-
-
 /* Fetch data from the API */
-function generatedColorAPI(getHex, getMode){
+function generatedColorAPI(getHex, getMode) {
     const url = "https://www.thecolorapi.com/scheme?" // Correct the hex value
     let hex = "hex=" + getHex
     let mode = "mode=" + getMode
-    let endpoint = hex+"&"+mode
+    let endpoint = hex + "&" + mode
     /* Fetch parameters */
     const param = {
         method: "GET",
@@ -42,31 +41,41 @@ function generatedColorAPI(getHex, getMode){
             "Content-Type": "application/json"
         }
     };
-    fetch(url+endpoint, param)
-    .then(res => {
-        if (!res.ok) {
-            throw new Error('Network response was not OK')  // Check for errors
-        }
-        return res.json()  // Parse JSON response
-    })
-    .then(data => {
-        getAllMode.push(data.mode)
-        getAllColors.push(data.colors)
-        renderColors()
-    })
-    .catch(error => console.error('There was an issue with the fetch request:', error))  // Handle errors
+    fetch(url + endpoint, param)
+        .then(res => {
+            if (!res.ok) {
+                throw new Error('Network response was not OK') // Check for errors
+            }
+            return res.json() // Parse JSON response
+        })
+        .then(data => {
+            getAllMode.push(data.mode)
+            getAllColors.push(data.colors)
+            renderColors()
+        })
+        .catch(error => console.error('There was an issue with the fetch request:', error)) // Handle errors
 }
-
 
 function renderColors() {
-    console.log("Colors:", getAllColors[0])  // Log the colors
-    getAllColors.forEach(color =>{
-        console.log(color)
+    getAllColors.forEach(color => {
         color.forEach(hexColors => {
-            console.log(hexColors.hex.value)
+            hexValues.push(hexColors.hex.value)
         })
-    },[0])
-    
+    }, [0])
+    colorsToUI()
+}
+
+function colorsToUI() {
+    document.getElementById('s2-1').style.backgroundColor = hexValues[0]
+    document.getElementById('s2-2').style.backgroundColor = hexValues[1]
+    document.getElementById('s2-3').style.backgroundColor = hexValues[2]
+    document.getElementById('s2-4').style.backgroundColor = hexValues[3]
+    document.getElementById('s2-5').style.backgroundColor = hexValues[4]
+
+    document.getElementById('s3-1').innerText = hexValues[0]
+    document.getElementById('s3-2').innerText = hexValues[1]
+    document.getElementById('s3-3').innerText = hexValues[2]
+    document.getElementById('s3-4').innerText = hexValues[3]
+    document.getElementById('s3-5').innerText = hexValues[4]
 
 }
-//----------------------------Testing----------------------
